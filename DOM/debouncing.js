659884f9e278b3cebe0c90f12449cc7aa5bff695
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-10-07 22:36:15 +0530

    Create debouncing.js

---
+++ DOM/debouncing.js
@@ -0,0 +1,22 @@
+//HTML
+<input type="text" onkeyup="betterFunction()">
+  
+ //JS
+  
+let counter=0;
+const getData=()=>{
+  //api call
+ console.log("fetching data",counter++) 
+}
+
+const doSomeMagic=function(fn,delay){
+ let timer;
+  return function(){
+  let context=this,
+      args=arguments;
+    clearTimeout(timer);
+    timer=setTimeout(()=>{
+    fn.apply(context,args)
+    },delay)
+  }
+}
