05b891085ff07ef432f78ac36f13a2df0e1039fc
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-10-10 15:31:36 +0530

    Update callback.js

--- DOM/ASYNC PROGRAMMING/callback.js
+++ DOM/ASYNC PROGRAMMING/callback.js
@@ -14,15 +14,9 @@ api.createOrder(cart,function (){
 
  //SAME CODE WITH PROMISE
                 
- createOrder(cart).then(function(orderId){
-return proceedToPayment(orderId)
-})
-.then(function(paymentInfo){
-    return showOrderSummary(paymentInfo)
-})
-.then(function(paymentInfo){
-    return updateWallet(paymentInfo)
-})
+ createOrder(cart).then((orderId)=>proceedToPayment(orderId))
+.then((paymentInfo)=>showOrderSummary(paymentInfo))
+.then((paymentInfo)=>updateWallet(paymentInfo))
                 
 //Problem with Callback is
  1.Callback hell(there will be deeply nested function aka pyramid of dom)
