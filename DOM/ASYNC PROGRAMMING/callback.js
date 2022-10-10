5778144b4d46bbe557fa02fbc5982c758bda8c97
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2022-10-10 10:48:07 +0530

    Update callback.js

--- DOM/ASYNC PROGRAMMING/callback.js
+++ DOM/ASYNC PROGRAMMING/callback.js
@@ -1,4 +1,20 @@
-//when a function accepts another function as a parameter
+//when a function accepts another function as a arguments
+
+const cart=["shoes","purse","phone"]
+
+api.createOrder(cart,function (){
+    api.proceedToPayment(function(){
+    api.showOrderSummary(
+        function(){
+            api.updateWallet()
+        }
+        )
+    })
+}
+
+//Problem with Callback is
+ 1.Callback hell(there will be deeply nested function aka pyramid of dom)
+ 2.Inversion of control(one function is completely dependent on another function)            
 
 //example setTimeout,setInterval,map
 
