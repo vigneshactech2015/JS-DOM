f4bd2d2c5e29694937116588cc0069be7b5a9c8d
Vignesh Vijayakumar <85957001+vigneshactech2015@users.noreply.github.com>
2024-07-27 11:26:36 +0530

    Update prototype.js

--- DOM/prototype.js
+++ DOM/prototype.js
@@ -1,13 +1,8 @@
-var person = function (y){
-  this.year=y
+String.prototype.myMethod = function(){
+  return this.toUpperCase()
 }
 
-person.prototype.calculateAge = function(){
-  return 2022-this.year
-}
-
-var person1 = new person(1997)
-console.log(person1.calculateAge())
+console.log("vignesh".myMethod())
 
 //All js objects derive its method and properties from prototype
 //using a prototype we can create new methods to the object
