21ccf9d858017f3f17857807daa2c4c741cafb51
Vignesh Vijayakumar <85957001+vigneshactech2015@users.noreply.github.com>
2025-01-03 10:12:54 +0530

    Update eventdelegation.js

--- DOM/eventdelegation.js
+++ DOM/eventdelegation.js
@@ -22,3 +22,21 @@ document.querySelector("#category").addEventListener('click',(e)=>{
 1.memory saving                     1.All Events are not bubbled up(Ex. Focus , blur)
 2.Less code complexity              2.Stop Propogation used means delegation will not work
 3.Less DOM Manipulation
+
+
+// React
+
+export default function App() {
+  const onClickHandler = (e) => {
+    console.log(e.target.id);
+  };
+  return (
+    <div className="App">
+      <ul id="Category" onClick={onClickHandler}>
+        <li id="Laptop">Laptop</li>
+        <li id="Mobile">Mobile</li>
+        <li id="AC">AC</li>
+      </ul>
+    </div>
+  );
+}
