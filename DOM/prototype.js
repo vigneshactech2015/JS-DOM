bf17159842709cf1b6a6cff787a0bc4bf568a4bf
Vignesh Vijayakumar <85957001+vigneshactech2015@users.noreply.github.com>
2025-06-12 10:25:16 +0530

    Update prototype.js

--- DOM/prototype.js
+++ DOM/prototype.js
@@ -6,3 +6,28 @@ console.log("vignesh".myMethod())
 
 //All js objects derive its method and properties from prototype
 //using a prototype we can create new methods to the datatypes
+
+Example : 
+
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayHello = function () {
+  console.log(`Hello, I'm ${this.name}`);
+};
+
+const p1 = new Person("Alice");
+
+p1.sayHello(); // Hello, I'm Alice
+
+In this example:
+
+p1 doesn’t have a sayHello method.
+
+But its prototype (Person.prototype) does.
+
+So JavaScript finds it up the prototype chain.
+
+Prototype chain :
+p1 --> Person.prototype --> Object.prototype --> null
